f15bbff2c48b921c7d7b8488972b4d1d211a2f89
Dao Khai <khaidaofpt@gmail.com>
2024-11-20 16:03:50 +0700

    dsadasd

--- admin/config.php
+++ admin/config.php
@@ -1,22 +1,6 @@
 <?php
-<<<<<<< HEAD
 define("DB_HOST", "localhost"); // Hoặc hostname của bạn
 define("DB_USER", "root"); // Username của MySQL
 define("DB_PASS", "khaikhai"); // Mật khẩu của MySQL
 define("DB_NAME", "web_duan1"); // Tên cơ sở dữ liệu
-=======
-// Định nghĩa các hằng số kết nối cơ sở dữ liệu
-if (!defined('DB_HOST')) {
-    define("DB_HOST", "localhost"); // Địa chỉ máy chủ cơ sở dữ liệu
-}
-if (!defined('DB_USER')) {
-    define("DB_USER", "root");      // Tên người dùng cơ sở dữ liệu
-}
-if (!defined('DB_PASS')) {
-    define("DB_PASS", "052005");  // Mật khẩu của người dùng
-}
-if (!defined('DB_NAME')) {
-    define("DB_NAME", "web_duan1"); // Tên cơ sở dữ liệu
-}
->>>>>>> 70c9a5a2c0c0c72a5d453bb7f6d49a50d345a652
 ?>
