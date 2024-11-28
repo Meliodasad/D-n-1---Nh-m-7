6a4ef64165334ea111746bf6abeb6691123452ec
Dao Khai <khaidaofpt@gmail.com>
2024-11-28 22:28:32 +0700

    dádasdasd

--- admin/productadd.php
+++ admin/productadd.php
@@ -13,57 +13,66 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // $insert_product = $product ->insert_product($_FOST,$_FILES);
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <div class="admin-content-right">
+        <div class="admin-content-right-product_add">
+            <h1>Thêm sản phẩm</h1>
+            <form action="" method="POST" enctype="multipart/form-data">
+                <label for="">nhập tên sản phẩm <span style="color: red;">*</span></label>
+                <input name="product_name" required type="text" >
+                <label for="">Chọn danh mục <span style="color: red;">*</span></label>
+                <select name="category_id" id="">
+                    <option value="#">-chọn-</option>
+                    <?php
+                    $show_category = $product -> show_category();
+                    if ($show_category) {while ($result = $show_category -> fetch_assoc()) {
 
-<div class="admin-content-right">
-<div class="admin-content-right-product_add">
-    <h1>Thêm sản phẩm</h1>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <label for="">nhập tên sản phẩm <span style="color: red;">*</span></label>
-        <input name="product_name" required type="text" >
-        <label for="">Chọn danh mục <span style="color: red;">*</span></label>
-        <select name="category_id" id="">
-            <option value="#">-chọn-</option>
-            <?php
-            $show_category = $product -> show_category();
-            if ($show_category) {while ($result = $show_category -> fetch_assoc()) {
+                        
+                    ?>
+                    <option value="<?php echo $result['category_id'] ?>"><?php echo $result['category_name'] ?></option>
+                    <?php
+                    }}
+                    ?>
+                </select>
+                <label for="">Chọn loại sản phẩm <span style="color: red;">*</span></label>
+                <select name="brand_id" id="">
+                    <label for="">chọn loại sản phẩm <span style="color: red;">*</span></label>
+                    <option value="#">-chọn-</option>
+                    <?php
+                    $show_brand = $product -> show_brand();
+                    if ($show_brand) {while ($result = $show_brand -> fetch_assoc()) {
 
-                
-            ?>
-            <option value="<?php echo $result['category_id'] ?>"><?php echo $result['category_name'] ?></option>
-            <?php
-            }}
-            ?>
-        </select>
-        <label for="">Chọn loại sản phẩm <span style="color: red;">*</span></label>
-        <select name="brand_id" id="">
-            <label for="">chọn loại sản phẩm <span style="color: red;">*</span></label>
-            <option value="#">-chọn-</option>
-            <?php
-            $show_brand = $product -> show_brand();
-            if ($show_brand) {while ($result = $show_brand -> fetch_assoc()) {
+                        
+                    ?>
+                    <option value="<?php echo $result['brand_id'] ?>"><?php echo $result['brand_name'] ?></option>
+                    <?php
+                    }}
+                    ?>
+                </select>
+                <label for="">giá sản phẩm <span style="color: red;">*</span></label>
+                <input name="product_price" required type="text" >
+                <label for="">giá khuyễn mãi <span style="color: red;">*</span></label>
+                <input name="product_price_new" required type="text" >
+                <label for="">mô tả sản phẩm <span style="color: red;">*</span></label>
+                <textarea required name="product_desc" id="" cols="30" rows="10"></textarea>
+                <label for="">ảnh sản phẩm <span style="color: red;">*</span></label>
+                <input name="product_img" required type="file" >
+                <label for="">ảnh mô tả <span style="color: red;">*</span></label>
+                <input name="product_img_desc[]" required multiple type="file" >
+                <button type="submit">Thêm</button>
+                <!-- required:nhắc nhở không được để trống  -->
+            </form>
 
-                
-            ?>
-            <option value="<?php echo $result['brand_id'] ?>"><?php echo $result['brand_name'] ?></option>
-            <?php
-            }}
-            ?>
-        </select>
-        <label for="">giá sản phẩm <span style="color: red;">*</span></label>
-        <input name="product_price" required type="text" >
-        <label for="">giá khuyễn mãi <span style="color: red;">*</span></label>
-        <input name="product_price_new" required type="text" >
-        <label for="">mô tả sản phẩm <span style="color: red;">*</span></label>
-        <textarea required name="product_desc" id="" cols="30" rows="10"></textarea>
-        <label for="">ảnh sản phẩm <span style="color: red;">*</span></label>
-        <input name="product_img" required type="file" >
-        <label for="">ảnh mô tả <span style="color: red;">*</span></label>
-        <input name="product_img_desc[]" required multiple type="file" >
-        <button type="submit">Thêm</button>
-        <!-- required:nhắc nhở không được để trống  -->
-    </form>
-
- </div>
+        </div>
 
     </section>
 </body>
